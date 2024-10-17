c5300ca739d05ceb61e16f3b06fb06a350624bc0
JospehP64 <jmjakepardo@gmail.com>
2024-10-17 21:30:37 +0200

    17-10-2024_1
    -Se ha corregido el error de vector2 y vector3 (se había usado un vector dos de otra librería en vez del vector2 de unity. TENER EN CUENTA ESTE DETALLE)

--- Assets/FirstPerson.cs
+++ Assets/FirstPerson.cs
@@ -1,7 +1,5 @@
 using System.Collections;
 using System.Collections.Generic;
-using System.Numerics;
-using Unity.Mathematics;
 using UnityEngine;
 
 public class FirstPerson : MonoBehaviour
@@ -24,18 +22,18 @@ public class FirstPerson : MonoBehaviour
         Cursor.lockState = CursorLockMode.Locked;
         float h = Input.GetAxisRaw("Horizontal");
         float v = Input.GetAxisRaw("Vertical");
-       // Vector2 input = new Vector2(h, v) * Mathf.Rad2Deg; CORREGIR
-        //if (input.magnitude > 0) CORREGIR
+        Vector2 input = new Vector2(h, v);
+        if (input.magnitude > 0)
         {
 
 
             //Para mover un objeto con un "Character controller", usa move()
             //Vector3 movement = new Vector3(h, 0, v).normalized;
 
-          //  angleRotation = Mathf.Atan2(input.x, input.y) * Mathf.Rad2Deg + Camera.main.transform.eulerAngles.y; CORREGIR
-          //  transform.eulerAngles = new Vector3(0, angleRotation, 0); CORREGIR
-          //  Vector3 movement = quaternion.Euler(0, angleRotation, 0) * Vector3.forward; CORREGIR
-            //CController.Move(movement * playerSpeed * Time.deltaTime); CORREGIR
+            angleRotation = Mathf.Atan2(input.x, input.y) * Mathf.Rad2Deg + Camera.main.transform.eulerAngles.y;
+            transform.eulerAngles = new Vector3(0, angleRotation, 0);
+            Vector3 movement = Quaternion.Euler(0, angleRotation, 0) * Vector3.forward;
+            CController.Move(movement * playerSpeed * Time.deltaTime);
         }
         
     }
