b9a4b6ee0150b3351d6025679c109fbbe420d876
JospehP64 <jmjakepardo@gmail.com>
2024-10-21 17:49:04 +0200

    21-10-2024
    -Se ha creado un script para la vista en primera persona
    -Se ha creado el movimiento en para el personaje en primera persona
    -Se ha creado una cámara en tercera persona
    -se ha añadido un modelo, animaciones y ontrolador de animaciones
    -Se han creado 3 estados (idel, walking1 y walking 2) para el controlador
    -Se ha creado un script para el movimiento del personaje en 3 persona

--- Assets/ThirdPerson.cs
+++ Assets/ThirdPerson.cs
@@ -1,28 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Mathematics;
 using UnityEngine;
 
-public class FirstPerson : MonoBehaviour
+public class ThirdPerson : MonoBehaviour
 {
     Camera MainCamera;//solo es para acortar el Camera.main
-    [SerializeField]float playerSpeed;
+    [SerializeField] float playerSpeed;
     [SerializeField] int vida = 3;
     CharacterController CController;
     [SerializeField] float angleRotation;
-    
+     [SerializeField] float SmoothingTime;
+    [SerializeField] float RotationSpeed;
+    Animator anima;
 
     // Start is called before the first frame update
     void Start()
     {
-       CController = GetComponent<CharacterController>();
+        CController = GetComponent<CharacterController>();
+        anima = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        Cursor.lockState = CursorLockMode.Locked;
-        float h = Input.GetAxisRaw("Horizontal");
-        float v = Input.GetAxisRaw("Vertical");
+        float h, v;
+        NewMethod(out h, out v);
+
         Vector2 input = new Vector2(h, v);
         if (input.sqrMagnitude > 0)
         {
@@ -32,10 +36,20 @@ public class FirstPerson : MonoBehaviour
             //Vector3 movement = new Vector3(h, 0, v).normalized;
 
             angleRotation = Mathf.Atan2(input.x, input.y) * Mathf.Rad2Deg + Camera.main.transform.eulerAngles.y;
+            float SmoothedAngle = Mathf.SmoothDampAngle(transform.eulerAngles.y, angleRotation, ref RotationSpeed, SmoothingTime);
             transform.eulerAngles = new Vector3(0, angleRotation, 0);
             Vector3 movement = Quaternion.Euler(0, angleRotation, 0) * Vector3.forward;
             CController.Move(movement * playerSpeed * Time.deltaTime);
         }
-        
+
+    }
+
+    private void NewMethod(out float h, out float v)
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        h = Input.GetAxisRaw("Horizontal");
+        v = Input.GetAxisRaw("Vertical");
+         
+        anima.SetBool("BoolWalking", true);
     }
 }
