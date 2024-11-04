344b811336d6eca82aa3e567a377369bb8a120d5
JospehP64 <jmjakepardo@gmail.com>
2024-11-04 17:30:52 +0100

    04-11-2024
    -Se han hecho cambios en las animaciones del enemigo (se han creado eventos de animacion)
    -Se han hecho cambios en el controlador del enemigo
    -Se ha modificado el prefab del enemigo
    -Aun queda por crear un boxcollider (EN EL PREFAB) para que, al atacar, el enemigo realize algún daño al jugador
    -POR FAVOR, HACERLO EN CASA. TENER EN CUENTA LOS EVENTOS DE ANIMACION, LOS METODOS Y VARIABLES TANTO DEL JUGADOR COMO DEL ENEMIGO.

--- Assets/Foe.cs
+++ Assets/Foe.cs
@@ -5,8 +5,11 @@ using UnityEngine.AI;
 
 public class Foe : MonoBehaviour
 {
+    [SerializeField] Transform attackPointPosition;
+    [SerializeField] float attackRadius;
 
     //Foe = Enemigo
+    Animator animate;
     FirstPerson Firstplayer;
     [SerializeField]GameObject player;
     NavMeshAgent agent;
@@ -16,11 +19,33 @@ public class Foe : MonoBehaviour
 
        agent = GetComponent<NavMeshAgent>();
         //player = GameObject.FindObjectOfType<FirstPerson>();//para que el enemigo persiga a su objetivo
+        animate = GetComponent<Animator>();
+        
     }
 
     // Update is called once per frame
     void Update()
     {
         agent.SetDestination(player.transform.position);
+
+        if(agent.remainingDistance <= agent.stoppingDistance)
+        {
+            agent.isStopped = true;
+            animate.SetBool("atacar", true);
+
+        }
+        [SerializeField]void AttackAnimationEnd()
+        {
+
+        }
+        [SerializeField] void AttackWindowEnd()
+        {
+
+        }
+        [SerializeField] void AttackWindowStart()
+        {
+            Physics.OverlapSphere(attackPointPosition.position, attackRadius);
+        }
+        
     }
 }
