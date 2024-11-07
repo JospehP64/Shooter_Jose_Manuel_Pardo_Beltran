fcfad2909c32a2963db0ce56b653c63ca33c3734
JospehP64 <jmjakepardo@gmail.com>
2024-11-07 21:28:12 +0100

    07-11-2024
    -Se ha creado un ScriptableObject  para guardar los datos de la pistolas sin depender del objeto (Es decir, es un objeto scripteable que permite usar datos creados (como "vida" y "distancia de alcance de arma") sin tener que usarlo dentro del script o crear instancias y prefabs)
    -RECUERDA: RAGDOLL SIRVE PARA QUE, LAS PARTES DEL ENEMIGO SELECCIONADAS, AL NO DESACTIVAR SUS COMPORTAMIENTOS, SE CAIGAN LOS HUESOS Y SE COMPORTE COMO UN CUERPO INMOVIL
    
    -RECUERDA TRABAJAR EN EL DISPARO DE LA PISTOLA Y EL DAÑO QUE PROVOCA AL ENEMIGO. NO LO DEJES PARA MÁS TARDE

---
+++ Assets/weapon.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class weapon : ScriptableObject 
+{
+    public ScriptableObject Arma;
+    // ScriptableObject 
+    public float distanciaDeAtaque;
+    public float danhoDeAtaque;
+    public int InerciaDeAtaque;
+}
